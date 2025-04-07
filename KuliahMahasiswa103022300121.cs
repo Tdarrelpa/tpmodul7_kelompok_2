0e9c543aa4edbf774bf3fde9aef8c5a0ca0e15b6
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-04-08 01:58:14 +0700

    menambahkan class KuliahMahasiswa103022300121

---
+++ KuliahMahasiswa103022300121.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static tpmodul07_kelompok02.DataMahasiswa103022300121;
+using static tpmodul07_kelompok02.KuliahMahasiswa103022300121;
+
+namespace tpmodul07_kelompok02
+{
+    class KuliahMahasiswa103022300121
+    {
+        public class Course
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+        public class CourseList
+        {
+            public List<Course> courses { get; set; }
+        }
+
+        public void ReadJSON(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            CourseList data = JsonSerializer.Deserialize<CourseList>(json);
+
+            int i = 1;
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine("MK " + i + " " + course.code + " - " + course.name);
+                i++;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using tpmodul07_kelompok02;
 
 DataMahasiswa103022300121 dataMahasiswa103022300121 = new DataMahasiswa103022300121();
+KuliahMahasiswa103022300121 kuliahMahasiswa103022300121 = new KuliahMahasiswa103022300121();
 
-
+// file json terletak pada nama_project\bin\Debug\net8.0
 dataMahasiswa103022300121.ReadJSON("tp7_1_103022300121.json");
+kuliahMahasiswa103022300121.ReadJSON("tp7_2_103022300121.json");
