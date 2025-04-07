99d88157fd62a00835164eb144acbf9e787607eb
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-04-08 01:36:17 +0700

    menambahkan class DataMahasiswa103022300121

---
+++ DataMahasiswa103022300121.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul07_kelompok02
+{
+    class DataMahasiswa103022300121
+    {
+        public class Nama
+        {
+            public string depan { get; set; }
+            public string belakang { get; set; }
+        }
+
+        public class Mahasiswa
+        {
+            public Nama nama { get; set; }
+            public long nim { get; set; }
+            public string fakultas { get; set; }
+        }
+
+        public void ReadJSON(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            Mahasiswa data = JsonSerializer.Deserialize<Mahasiswa>(json);
+            Console.WriteLine("Nama " + data.nama.depan + " " + data.nama.depan + " dengan nim " + data.nim + " dari fakultas " + data.fakultas);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul07_kelompok02;
+
+DataMahasiswa103022300121 dataMahasiswa103022300121 = new DataMahasiswa103022300121();
+
+
+dataMahasiswa103022300121.ReadJSON("tp7_1_103022300121.json");
