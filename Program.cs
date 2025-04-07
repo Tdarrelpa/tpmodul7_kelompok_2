ddaf54f56527ad486e381273bc0b94a435fb3819
Tdarrelpa <darrel2208@gmail.com>
2025-04-07 23:48:26 +0700

    Menambahkan class KuliahMahasiswa_103022300082

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using cobaJSON1;
+using cobaJSON2;
 
 public class Program
 {
@@ -8,5 +9,8 @@ public class Program
         string jsonFilePath = "C:\\Users\\darrel\\Documents\\tpmodul7_kelompok_2\\tp7_1_103022300082.json"; // Specify path to JSON1
         DataMahasiswa_103022300082 mahasiswa = DataMahasiswa_103022300082.ReadJSON(jsonFilePath);
         mahasiswa.PrintMahasiswa();
+
+        KuliahMahasiswa_103022300082 kuliahMahasiswa = new KuliahMahasiswa_103022300082();
+        kuliahMahasiswa.ReadJSON("C:\\Users\\darrel\\Documents\\tpmodul7_kelompok_2\\tp7_2_103022300082.json");
     }
 }
---
+++ cobaJSON2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace cobaJSON2 
+{
+    public class Course
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class KuliahMahasiswa_103022300082
+    {
+        public List<Course> Courses { get; set; }
+
+        public void ReadJSON(string filePath)
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                KuliahMahasiswa_103022300082 kuliahMahasiswa = JsonSerializer.Deserialize<KuliahMahasiswa_103022300082>(jsonString);
+
+                Console.WriteLine("Daftar mata kuliah yang diambil:");
+                for (int i = 0; i < kuliahMahasiswa.Courses.Count; i++)
+                {
+                    Console.WriteLine($"MK {i + 1} {kuliahMahasiswa.Courses[i].Code} - {kuliahMahasiswa.Courses[i].Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
