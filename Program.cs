8cf935d272b44ddecb39511d40eb942f88233787
Tdarrelpa <darrel2208@gmail.com>
2025-04-07 22:49:11 +0700

    Menambahkan class DataMahasiswa_103022300082

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using cobaJSON1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string jsonFilePath = "C:\\Users\\darrel\\Documents\\tpmodul7_kelompok_2\\tp7_1_103022300082.json"; // Specify path to JSON1
+        DataMahasiswa_103022300082 mahasiswa = DataMahasiswa_103022300082.ReadJSON(jsonFilePath);
+        mahasiswa.PrintMahasiswa();
+    }
+}
---
+++ cobaJSON1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+// using Newtonsoft.Json;
+
+namespace cobaJSON1 
+{
+    public class DataMahasiswa_103022300082
+    {
+        //public string NIM { get; set; }
+        //public string Namadepan { get; set; }
+        //public string Namabelakang { get; set;}
+        //public string Fakultas { get; set; }
+
+        public string NIM { get; set; }
+        public string Namadepan { get; set; }
+        public string Namabelakang { get; set; }
+        public string Fakultas { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public DataMahasiswa_103022300082(){}
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        //public string GetNIM() { return nim; }
+        //public void SetNIM(string nim) { this.nim = nim; }
+        //public string GetNamaDepan() { return namadepan; }
+        //public void SetNamaDepan(string namadepan) { this.namadepan = namadepan; }
+        //public string GetNamaBelakang() { return namabelakang; }
+        //public void SetNamaBelakang(string namabelakang) { this.namabelakang = namabelakang; }
+        //public string GetFakultas() { return fakultas; }
+        //public void SetFakultas(string fakultas) { this.fakultas = fakultas; }
+
+        public static DataMahasiswa_103022300082 ReadJSON(string filePath)
+        {
+                string jsonData = File.ReadAllText(filePath);
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            DataMahasiswa_103022300082 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_103022300082>(jsonData);
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                              // DataMahasiswa_103022300082 mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa_103022300082>(jsonData);
+            return mahasiswa;
+                // return JsonSerializer.Deserialize<DataMahasiswa>(jsonData);
+        }
+
+        public void PrintMahasiswa()
+        {
+            Console.WriteLine($"Nama {Namadepan} {Namabelakang} dengan nim {NIM} dari fakultas {Fakultas}");
+        }
+    }
+}
